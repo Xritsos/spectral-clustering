9336d4b7c24c5aac75f6b591aef585324720956a
Xritsos <kobecris24@yahoo.com>
2024-01-05 17:09:45 +0200

    added plots

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import kneighbors_graph
 from scipy.linalg import eigh, eig
 from numpy import linalg
 from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
 import warnings
 from scipy.sparse.linalg import eigsh
 import networkx as nx
@@ -41,11 +42,13 @@ def read_data():
 
     x1 = df['calories'].to_numpy()
     x2 = df['fat'].to_numpy()
+    x3 = df['weight'].to_numpy()
     
     x1 = np.expand_dims(x1, axis=1)
     x2 = np.expand_dims(x2, axis=1)
+    x3 = np.expand_dims(x3, axis=1)
     
-    x = np.concatenate((x1, x2), axis=1)
+    x = np.concatenate((x1, x2, x3), axis=1)
     
     return x
 
@@ -89,20 +92,20 @@ def calculate_eigenmaps(neighbors, data):
     D = np.sqrt(D)
     
     # normalized Laplacian
-    laplacian_rw = np.matmul(np.matmul(D, laplacian), D)
+    laplacian_rw = np.dot(np.dot(D, laplacian), D)
     
     
     sum_ = []
-    for i in range(laplacian_rw.shape[0]):
-        sum_.append(np.sum(laplacian_rw[i, :]))
+    for i in range(laplacian.shape[0]):
+        sum_.append(np.sum(laplacian[i, :]))
     
     print("Laplacian row sum")
     print(sorted(sum_))
     print()
     
     sum_ = []
-    for i in range(laplacian_rw.shape[1]):
-        sum_.append(np.sum(laplacian_rw[:, i]))
+    for i in range(laplacian.shape[1]):
+        sum_.append(np.sum(laplacian[:, i]))
     
     print("Laplacian col sum")
     print(sorted(sum_))
@@ -114,8 +117,8 @@ def calculate_eigenmaps(neighbors, data):
     # print("Laplacian")
     # print(laplacian)
     
-    eigen_values, eigen_vectors = eigh(laplacian_rw)
-    # eigen_values, eigen_vectors = eigh(laplacian, D)
+    # eigen_values, eigen_vectors = eigh(laplacian_rw)
+    eigen_values, eigen_vectors = eigh(laplacian, D)
     
     # # # # sort values and vectors
     # # # idxs = np.argsort(eigen_values)
@@ -129,18 +132,52 @@ def calculate_eigenmaps(neighbors, data):
 
 if __name__ == "__main__":
     x = read_data()
+   
+    # fig = plt.figure()
     
+    # plt.scatter(x[:, 0], x[:, 1])
+    
+    # plt.show() 
+    
+      
     print(x.shape)
     # neighbors = int(np.ceil(np.sqrt(x.shape[0])))
-    neighbors = 3
+    neighbors = 9
     print()
     print(f"Number of Neighbors: {neighbors}")
     
-    scaler = MinMaxScaler()
+    scaler = StandardScaler()
     x = scaler.fit_transform(x)
+    
+    print(x[:, 0].shape)
+    print(x[:, 1].shape)
+    print(x[:, 2].shape)
+    
+    fig = plt.figure(figsize = (10, 7))
+    ax = plt.axes(projection ="3d")
+ 
+# Create Plot
+
+    ax.scatter3D(x[:, 0], x[:, 1], x[:, 2])
+ 
+# Show plot
+
+    plt.show()
     
     eigen_values, eigen_vectors = calculate_eigenmaps(neighbors, x)
     # calculate_eigenmaps(neighbors, x)
     
-    print(eigen_values)
+    eigen_vectors = eigen_vectors[eigen_values > 1e-14, :]
+    eigen_values = eigen_values[eigen_values > 1e-14]
+    
+    #eigen_values = eigen_values[:2]
+    eigen_vectors = eigen_vectors[:, :2]
+    
+    fig = plt.figure()
+    
+    plt.scatter(eigen_vectors[:, 0], eigen_vectors[:, 1])
+    
+    plt.show() 
+    
+   
     
