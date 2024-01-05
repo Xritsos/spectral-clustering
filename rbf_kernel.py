17e237c0fb5de55ad91c9c217df4d2f4b44db664
Xritsos <kobecris24@yahoo.gr>
2024-01-05 23:50:06 +0200

    rbf_kernel

---
+++ rbf_kernel.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def rbf(x1, x2, gamma):
+    
+    n = x1.shape[0]
+    m = x2.shape[0]
+    
+    xx1 = np.dot(np.sum(np.power(x1, 2), 1).reshape(n, 1), np.ones((1, m)))
+    xx2 = np.dot(np.sum(np.power(x2, 2), 1).reshape(m, 1), np.ones((1, n))) 
+    
+    result = np.exp(-(xx1 + xx2.T - 2 * np.dot(x1, x2.T)) * gamma) 
+    
+    return result
