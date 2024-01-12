546c299640dfd866699cb3c630f5914d4aff251f
Xritsos <kobecris24@yahoo.gr>
2024-01-12 12:19:49 +0200

    added number of images to load option

--- River/load_river.py
+++ River/load_river.py
@@ -5,14 +5,13 @@ from matplotlib import pyplot as plt
 
 
 
-def load():
+def load(n_images):
     
     images = np.zeros((10, 64, 64, 3), dtype=np.int64)
     for i, image in enumerate(glob.glob('./River/images/*')):
-        print(image)
         im_frame = Image.open(image)
         x = np.array(im_frame)
         
         images[i, :, :, :] = x
         
-    return images
+    return images[:n_images, :, :, :]
